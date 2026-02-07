27a47e579ba03643a6a6f886390d33db33002ee4
Chaitanya <your_email@example.com>
2026-02-07 13:36:59 +0530

    Initial commit: Spring Boot 4 + Auth Dependencies

---
+++ src/main/java/com/example/AuthyAutho/AuthyAuthoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthyAutho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthyAuthoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthyAuthoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthyAutho/AuthyAuthoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthyAutho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthyAuthoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
